204252e66d517a23b23ae95ac90cbbf241df3163
Jaid <jaid.jsx@gmail.com>
2019-05-26 11:16:14 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-/** @module essential-config */
+import path from "path"
+import fs from "fs"
 
-/**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
- * @example
- * import essentialConfig from "essential-config"
- * essentialConfig()
- * // 1549410770
- * @returns {number} Seconds since epoch
- */
-export default () => Math.floor(Date.now() / 1000)
+import fss from "@absolunet/fss"
+import appdataPath from "appdata-path"
+
+export default (name, defaultConfig) => {
+  const appFolder = appdataPath(name)
+  const defaultConfigFile = path.join(appFolder, "config.default.yml")
+  fss.outputYaml(defaultConfigFile, defaultConfig)
+  const configFile = path.join(appFolder, "config.yml")
+  if (!fs.existsSync(configFile)) {
+    fss.outputYaml(configFile, defaultConfig)
+    return false
+  }
+  return fss.readYaml(configFile)
+}
--- test/test.js
+++ test/test.js
@@ -1,9 +1,23 @@
 import path from "path"
+import fs from "fs"
+
+import appdataPath from "appdata-path"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 const {default: essentialConfig} = indexModule
 
 it("should run", () => {
-  const result = essentialConfig()
-  expect(result).toBeGreaterThan(1549410770)
+  const id = `${_PKG_NAME}-test`
+  essentialConfig(id, {
+    car: {
+      color: "red",
+      speed: 522,
+    },
+  })
+  for (const fileName of ["config.yml", "config.default.yml"]) {
+    const configFile = path.join(appdataPath(id), fileName)
+    expect(fs.existsSync(configFile)).toBeTruthy()
+    const content = fs.readFileSync(configFile, "utf8")
+    expect(content.length).toBeGreaterThan(10)
+  }
 })
