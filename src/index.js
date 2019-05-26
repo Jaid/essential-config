48df22bdeb4c7c3f696f47acd8c7cf02f1bc9017
Jaid <jaid.jsx@gmail.com>
2019-05-26 13:08:13 +0200

    Added check for missing keys at every startup
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import fs from "fs"
 
 import fss from "@absolunet/fss"
 import appdataPath from "appdata-path"
+import {difference} from "lodash"
+import sortKeys from "sort-keys"
 
 export default (name, defaultConfig) => {
   const appFolder = appdataPath(name)
@@ -10,8 +12,17 @@ export default (name, defaultConfig) => {
   fss.outputYaml(defaultConfigFile, defaultConfig)
   const configFile = path.join(appFolder, "config.yml")
   if (!fs.existsSync(configFile)) {
-    fss.outputYaml(configFile, defaultConfig)
+    fss.outputYaml(configFile, defaultConfig |> sortKeys)
     return false
   }
-  return fss.readYaml(configFile)
+  const config = fss.readYaml(configFile)
+  const givenKeys = Object.keys(config)
+  const defaultKeys = Object.keys(defaultConfig)
+  const missingKeys = difference(defaultKeys, givenKeys)
+  if (missingKeys.length) {
+    for (const missingKey of missingKeys) {
+      config[missingKey] = defaultConfig[missingKey]
+    }
+    fss.outputYaml(configFile, config |> sortKeys)
+  }
 }
